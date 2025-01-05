d8d2fcd69e6e7d96592b19470b7cb15da8088d20
davidl <jordan@sweethomeservices.org>
2025-01-05 23:49:11 +0200

    change migration

--- src/migrations/1735686254215-RemoveClientFromCampaign.ts
+++ src/migrations/1735686254215-RemoveClientFromCampaign.ts
@@ -5,11 +5,11 @@ export class RemoveClientFromCampaign1735686254215 implements MigrationInterface
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         
-        await queryRunner.query(`ALTER TABLE "campaigns" DROP COLUMN "clientId"`);
+        
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "campaigns" ADD "clientId" uuid`);
+    
        
     }
 
