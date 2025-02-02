5fd9e86649d23e2c55f1aae0bb71871476f614df
davidl <jordan@sweethomeservices.org>
2025-02-02 17:07:09 +0200

    fix s3 dependecy

--- src/modules/knowledge-base/knowledge-base.module.ts
+++ src/modules/knowledge-base/knowledge-base.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { KnowledgeBase } from '../../entities/knowledgebase.entity';
 import { KnowledgeBaseController } from './knowledge-base.controller';
 import { KnowledgeBaseService } from './knowledge-base.service';
+import { S3Service } from './s3.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([KnowledgeBase])],
   controllers: [KnowledgeBaseController],
-  providers: [KnowledgeBaseService],
+  providers: [KnowledgeBaseService, S3Service,],
   exports: [KnowledgeBaseService],
 })
 export class KnowledgeBaseModule {}
